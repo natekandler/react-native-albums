e838176cea77e728b84f4fbfc6cb9ba58432c978
natekandler <natekandler@gmail.com>
2019-03-04 18:04:59 -0800

    add card and card section

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-class AlbumList extends React.Component {
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentDidMount() {
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => {
+        this.setState({ albums: response.data });
+        console.log(response);
+      });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     return (
       <View>
         <Text>album list</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
